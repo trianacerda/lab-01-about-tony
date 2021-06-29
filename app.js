b204d2d5f808615f9943b73ae2adfffa61f75d15
Triana Cerda <trianaRcerda@gmail.com>
2021-06-29 11:06:36 -0700

    added 8 ans vars

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-// import functions and grab DOM elements
+//const confirmed = confirm ("hello");
+//console.log(confirmed);
+//const myName = prompt("Am I named after a cereal?");
+//console.log(myName);
 
-// initialize state
+//get btn element
+//add "click" event listener to btn
+//confirm user wants to start the quiz
+//if yes-prompt for 8 questions and store answers
+//for each answer, check if the user entered a correct value
+//track the number of correct answers
+//end of quiz-update the results section w/ # of correct answers
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const qBtn = document.getElementById('btn');
+const results = document.getElementById('results');
+
+qBtn.addEventListener('click', ()=>{
+const confirmed =confirm('Would you like to start the Tony quiz?');
+console.log(confirmed);
+if (confirmed){
+const name = prompt('What is your name?');
+const ans1 = prompt('');
+const ans2 = prompt('');
+const ans3 = prompt('');
+const ans4 = prompt('');
+const ans5 = prompt('');
+const ans6 = prompt('');
+const ans7 = prompt('');
+const ans8 = prompt('');
+    console.log(name, ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8);
+  }
+    else {
+      console.log('user cancelled');
+    }
+});
